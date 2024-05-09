f9fbba4168afd9132a0fa9948721bdbd01c99428
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-05-09 21:28:33 +0700

    refactor: folder

---
+++ app/src/main/java/com/bookstore/app/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.bookstore.app.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bookstore.app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
